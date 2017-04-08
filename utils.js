9fb579e401b3f1e3ca558ee4b18aaa8945c6af8c
Peter Schlamp <p.schlamp@westernhealth.com>
2017-04-07 19:17:22 -0700

    init commit

---
+++ utils.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Created by joonkukang on 2014. 1. 18..
+ */
+var utils = module.exports;
+
+utils.math = require('./math');
+
+utils.isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
