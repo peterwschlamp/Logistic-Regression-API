e7f1ae0427e268549bbf0dc35763af04e7b458e8
Peter Schlamp <p.schlamp@westernhealth.com>
2017-04-07 20:02:52 -0700

    chore: updated output

--- index.js
+++ index.js
@@ -3,11 +3,6 @@
 var ml = module.exports;
 ml.LogisticRegression = require('./LogisticRegression');
 
-/**
- * Created by joonkukang on 2014. 1. 15..
- * https://github.com/junku901/machine_learning/blob/master/examples/LogisticRegression.js
- */
-//var ml = require('./lib/machine_learning');
 var x = [[1, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 1, 0]];
 var y = [[1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1]];
 
@@ -20,8 +15,7 @@ var classifier = new ml.LogisticRegression({
 //classifier.set('log level', 0);
 classifier.set('log level', 0);
 
-var training_epochs = 900,
-  lr = 0.01;
+var training_epochs = 900, lr = 0.01;
 
 classifier.train({
   'lr': lr,
@@ -32,4 +26,4 @@ x = [[1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0], [1, 1, 1, 1, 1, 0]];
 
 //console.log("Result : ", classifier.predict(x));
 var result = classifier.predict(x);
-console.log( "Score: ", result.map(function(r){return r[0]}) );
+console.log( "Score: ", result.map(function(r){return parseInt(r[0]*100)+'%'}) );
