d32ddbbfd89f9e8a84ed6e4a1b676bf31975c0c9
peter <peterwschlamp@gmail.com>
2017-04-09 09:11:52 -0700

    chore: refactored by moving code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
 let ml = module.exports;
 ml.LogisticRegression = require('./LogisticRegression');
+getLogReg = require('./src/getLogReg');
 
 
 
@@ -11,7 +12,8 @@ server.route({
     method: 'GET',
     path: '/hello/{user}',
     handler: function (request, reply) {
-        reply('Hello ' + encodeURIComponent(request.params.user) + '!');
+        //reply('Hello ' + encodeURIComponent(request.params.user) + '!');
+        reply(getLogReg());
     }
 });
 
@@ -22,28 +24,3 @@ server.start((err) => {
     }
     console.log(`Server running at: ${server.info.uri}`);
 });
-
-var x = [[1, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 1, 0]];
-var y = [[1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1]];
-
-var classifier = new ml.LogisticRegression({
-  'input': x,
-  'label': y,
-  'n_in': 6,
-  'n_out': 2
-});
-//classifier.set('log level', 0);
-classifier.set('log level', 0);
-
-var training_epochs = 900, lr = 0.01;
-
-classifier.train({
-  'lr': lr,
-  'epochs': training_epochs
-});
-
-x = [[1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0], [1, 1, 1, 1, 1, 0]];
-
-//console.log("Result : ", classifier.predict(x));
-var result = classifier.predict(x);
-console.log( "Score: ", result.map((r) => parseInt(r[0]*100)+'%') );
---
+++ src/getLogReg.js
@@ -0,0 +1,30 @@
+let ml = module.exports;
+ml.LogisticRegression = require('../LogisticRegression');
+
+module.exports = function getLogReg(){
+	var x = [[1, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 1, 0]];
+	var y = [[1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1]];
+
+	var classifier = new ml.LogisticRegression({
+	  'input': x,
+	  'label': y,
+	  'n_in': 6,
+	  'n_out': 2
+	});
+	//classifier.set('log level', 0);
+	classifier.set('log level', 0);
+
+	var training_epochs = 900, lr = 0.01;
+
+	classifier.train({
+	  'lr': lr,
+	  'epochs': training_epochs
+	});
+
+	x = [[1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0], [1, 1, 1, 1, 1, 0]];
+
+	//console.log("Result : ", classifier.predict(x));
+	var result = classifier.predict(x);
+	console.log( "Score: ", result.map((r) => parseInt(r[0]*100)+'%') );
+	return "Scores: " + result.map((r) => parseInt(r[0]*100)+'%');
+}
