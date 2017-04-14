c17cf9458025f3e6dd9d4d638f24ead08073771c
bruce <johnqin2006@gmail.com>
2017-04-14 16:01:26 +0100

    chore: added another endpoint

--- src/predict/predict.js
+++ src/predict/predict.js
@@ -5,8 +5,9 @@ var Joi = require('joi')
 
 var  payload =
     {
-        email:Joi.string().email().required(),
-        age   :Joi.number().integer().min(1).max(100).default(20)
+        email :Joi.string().email().required(),
+        age   :Joi.number().integer().min(1).max(100).default(20),
+        input :Joi.object().required()
     }
 /**
  * Created by bruceq on 13/04/17.
@@ -14,12 +15,12 @@ var  payload =
 module.exports.predict = function(request) {
     console.log("ha");
 
-    var error=error||{};
+    var error="";
     var err =Joi.validate(request.payload,payload,error);
 if (err.error==null)
 {
 
-  return "email: "+ request.payload.email + "age: "+request.payload.age +" result pass";
+  return "email: "+ request.payload.email + " age: "+request.payload.age +" result pass";
 }
 else {
     return err;
