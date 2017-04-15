1383a595793b81fd0c50ed42b2397b59e44dfd86
Peter Schlamp <p.schlamp@westernhealth.com>
2017-04-15 06:43:44 -0700

    refac: light refactoring on requires

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const Hapi = require('hapi');
 let ml = module.exports;
 ml.LogisticRegression = require('./src/LogisticRegression');
-getLogReg = require('./src/getLogReg');
 train=require('./src/training/train');
 predict=require('./src/predict/predict');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
 
-server.route({
-    method: 'GET',
-    path: '/hello/{user}',
-    handler: function (request, reply) {
-        //reply('Hello ' + encodeURIComponent(request.params.user) + '!');
-        reply(getLogReg());
-    }
-});
 server.route({
     method:'POST',
     path:'/train',
--- src/predict/predict.js
+++ src/predict/predict.js
@@ -1,13 +1,10 @@
-require('../getLogReg');
-let Joi = require('joi')
-
-let payload = {
-  input :Joi.object().required()
-}
-/**
- * Created by bruceq on 13/04/17.
- */
 module.exports.predict = function(request) {
+  let Joi = require('joi')
+  let getLogReg = require('../getLogReg');
+
+  let payload = {
+    input :Joi.object().required()
+  }
   console.log("ha");
   let error="";
   let err =Joi.validate(request.payload,payload,error);
