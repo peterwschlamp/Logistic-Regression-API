24b82273a686ac1a57d5231a0df8a5c2841a5d88
Peter Schlamp <p.schlamp@westernhealth.com>
2017-04-17 13:48:25 -0700

    chore: added new endpoints

--- index.js
+++ index.js
@@ -15,23 +15,68 @@ server.route({
     handler:function(request,reply){
        reply (train.train(request));
     }
-
 })
 
 server.route({
-    method:'POST',
-    path:'/predict',
-    handler:function(request,reply){
-        reply (predict.predict(request));
-    },
-    config: {
-      validate: {
-        params: {
-          data: Joi.object()
-        }
+  method:'POST',
+  path:'/train/test',
+  handler:function(request,reply){
+    reply (train.train(request));
+  }
+})
+
+server.route({
+  method:'POST',
+  path:'/train/test/{object_id}',
+  handler:function(request,reply){
+    reply (train.train(request));
+  }
+})
+
+server.route({
+  method:'POST',
+  path:'/train/list',
+  handler:function(request,reply){
+    reply (train.train(request));
+  }
+})
+
+server.route({
+  method:'POST',
+  path:'/train/list/{object_id}',
+  handler:function(request,reply){
+    reply (train.train(request));
+  }
+})
+
+server.route({
+  method:'POST',
+  path:'/predict',
+  handler:function(request,reply){
+    reply (predict.predict(request));
+  },
+  config: {
+    validate: {
+      params: {
+        data: Joi.object()
       }
     }
+  }
+})
 
+server.route({
+  method:'POST',
+  path:'/predict/{id}',
+  handler:function(request,reply){
+    reply (predict.predict(request));
+  },
+  config: {
+    validate: {
+      params: {
+        data: Joi.object()
+      }
+    }
+  }
 })
 
 server.start((err) => {
